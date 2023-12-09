ef98e0f070d33a309c760e504fa43b699e3e28a2
SkrivanekPeter <123364568+SkrivanekPeter@users.noreply.github.com>
2023-12-09 23:08:26 +0000

    Q1C and Q1D

---
+++ CA2-OOP/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CA2_OOP
+{
+    internal class Player
+    {
+        //Variables
+        public string Name { get; set; }
+        public string ResultRecord { get; set; }
+
+        //Constructors
+        public Player() { }
+    }
+}
